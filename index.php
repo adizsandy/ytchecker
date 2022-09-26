6951e213a0ffaed18fe5e3b89b54ceddc1d139f5
Shudhansh Shekhar <adizsandy@users.noreply.github.com>
2022-09-26 20:55:44 +0530

    d

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 
 $data = file_get_contents(__DIR__ . '/storage/data.json', true);
 
-require __DIR__ . '/src/controller/ytview.php';
+require __DIR__ . '/src/controller/ytsession.php';
+
+require __DIR__ . '/src/controller/ytaction.php';
+
+$_SESSION['_report_key_1'] = random_bytes(16);
+
+require __DIR__ . '/views/list.php';
