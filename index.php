98bba9d0cb045e07545ace1195a9a94b18b6dc59
Shudhansh Shekhar <adizsandy@users.noreply.github.com>
2022-09-26 20:48:27 +0530

    s

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
 $data = file_get_contents(__DIR__ . '/storage/data.json', true);
-print_r($data);
 
-require __DIR__ . '/src/controller/ytchecker.php';
+require __DIR__ . '/src/controller/ytview.php';
