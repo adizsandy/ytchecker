ba9c063b8ff512c080c2fd148ae3cb474126edb5
Shudhansh Shekhar <adizsandy@users.noreply.github.com>
2022-09-28 17:18:32 +0530

    sds

--- channel/action/all-list-fetch.php
+++ channel/action/all-list-fetch.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$result = return ['data' => null, 'message' => 'Something is wrong'];
+$result = ['data' => null, 'message' => 'Something is wrong'];
 
 try {
     if (isset($_POST['reportKey'])) {
