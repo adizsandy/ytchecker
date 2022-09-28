406bd587800ab4d2f983e0896f833e7b13f1aea8
Shudhansh Shekhar <adizsandy@users.noreply.github.com>
2022-09-28 19:30:56 +0530

    ddd

--- channel/action/all-list-submission.php
+++ channel/action/all-list-submission.php
@@ -4,21 +4,23 @@ require __DIR__ . '/../../src/Request/Request.php';
 
 $request = new Request();
 
+$result = ['data' => null, 'status' => false, 'message' => 'Something is wrong'];
+
 try {
     if ($request->isAjax()) {
         if ($request->get('reportKey') && $request->get('newId', 'string')) {
-            $key = $request->get('reportKey');
-            if ($key !== $_SESSION['_report_key_1']) {
-                throw new Exception("CSRF token error");
-            }
+            // $key = $request->get('reportKey');
+            // if ($key !== $_SESSION['_report_key_1']) {
+            //     throw new Exception("CSRF token error");
+            // }
 
-            $data = file_get_contents(__DIR__ . '/../../storage/data.json', true);
+            $data = json_decode(file_get_contents(__DIR__ . '/../../storage/data.json', true));
             $new_id = $request->get('newId', 'string');
             if (!in_array($new_id, $data, true)) {
                 $data[] = $new_id;
                 file_put_contents(__DIR__ . '/../../storage/data.json', $data);
 
-                return ['data' => null, 'message' => 'Success'];
+                $result = ['data' => null, 'status' => true, 'message' => 'Success'];
             } else {
                 throw new Exception("id already exists");
             }
@@ -26,7 +28,7 @@ try {
     }
     throw new Exception("Invalid request");
 } catch(Exception $e) {
-    return ['data' => null, 'message' => $e->getMessage()];
+    $result = ['data' => null, 'status' => false, 'message' => $e->getMessage()];
 }
 
-exit;
+echo json_encode($result);
