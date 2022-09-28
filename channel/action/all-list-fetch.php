65cd5269465ebc691e6025842ef96e07e39c374c
Shudhansh Shekhar <adizsandy@users.noreply.github.com>
2022-09-28 19:09:31 +0530

    aa

--- channel/action/all-list-fetch.php
+++ channel/action/all-list-fetch.php
@@ -4,16 +4,18 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-$result = ['data' => null, 'message' => 'Something is wrong'];
+$result = ['data' => null, 'status' => false, 'message' => 'Something is wrong'];
 
 try {
-    if (isset($_POST['reportKey'])) {
-        $data = file_get_contents(__DIR__ . '/../../storage/data.json', true);
-        $result = ['data' => $data, 'message' => 'Success'];
+    if (!isset($_POST['reportKey'])) {
+        $result = ['data' => null, 'status' => false, 'message' => 'Error! Invalid request']; 
     }
-    throw new Exception("Invalid request");
+
+    $data = file_get_contents(__DIR__ . '/../../storage/data.json', true);
+    $result = ['data' => $data, 'status' => true, 'message' => 'Success'];
+
 } catch(Exception $e) {
-    $result = ['data' => null, 'message' => $e->getMessage()];
+    $result = ['data' => null, 'status' => false, 'message' => $e->getMessage()];
 }
 
 echo json_encode($result);
